07b086150c055ed7233e0834e9af62ebe61359dd
Vicente Escarate <vicente.escarate.l@mail.pucv.cl>
2024-11-11 13:23:31 -0300

    Creacion del repositorio para la realizacion del trabaoj de optimizacion, adjunto el README donde se indicaran las dependencias a utilizar y un breve codigo que se podra copiar y pegar para instalarlas automaticament

---
+++ # inicio dle trabajo de optimizacion.py
@@ -0,0 +1,9 @@
+# inicio dle trabajo de optimizacion
+import math
+import itertools
+
+#Iniciare con una lista pequeña.
+listaClientes=[(2, 3), (5, 4), (7, 2), (8, 8), (1, 5), (6, 6)]  
+deposito=(0,0)
+capacidadMaxima=1445
+clientesMaximos=76
